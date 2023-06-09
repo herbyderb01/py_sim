e5f0e736990ec49c7dab5ac9d191b08c0da81353
Greg Droge <greg.droge@usu.edu>
2023-06-09 17:14:13 -0600

    Update rrt functions

--- py_sim/path_planning/rrt_planner.py
+++ py_sim/path_planning/rrt_planner.py
@@ -4,14 +4,13 @@
     by James Swedeen, Greg Droge, and Randall Christensen
 """
 
-from typing import Any
+from typing import Any, Optional, cast
 
 import numpy as np
 import numpy.typing as npt
 from py_sim.path_planning.graph_search import DirectedPathGraph as Tree
 from py_sim.path_planning.graph_search import World
 from py_sim.tools.sim_types import TwoDimArray
-from typing import Optional, cast
 
 Cost = dict[int, float] # Storage structure for storing the cost of a node in the graph
                         # it maps from node index to cost
@@ -33,7 +32,20 @@ class StateSpace:
         if x_lim[1] < x_lim[0] or y_lim[1] < y_lim[0]:
             raise ValueError("Limits must be increasing")
 
+    def contains(self, state: TwoDimArray) -> bool:
+        """ Evaluates if the state is in the state space. Returns true if it is
 
+            Inputs:
+                state: State to be evaluated
+
+            Outputs:
+                True if the state is in the state space, false otherwise
+        """
+        return bool(state.x >= self.x_lim[0] and state.x <= self.x_lim[1] and \
+                    state.y >= self.y_lim[0] and state.y <= self.y_lim[1])
+
+
+################ Basic Proceedures #################
 def initialize(root: TwoDimArray) -> tuple[Tree, Cost]:
     """Returns an initialized tree with the given root and no edges
 
@@ -84,7 +96,7 @@ def biased_sample(iteration: int, bias_t: int, X: StateSpace, X_t: StateSpace) -
         return sample(X=X_t)
     return sample(X=X)
 
-def nearest(x: TwoDimArray, tree: Tree) -> tuple[TwoDimArray, float]:
+def nearest(x: TwoDimArray, tree: Tree) -> tuple[TwoDimArray, int]:
     """Returns the state in the tree nearest to x
 
         Inputs:
@@ -297,7 +309,7 @@ def cost_to_come(x_n: TwoDimArray, ind_p: int, tree: Tree, cost: Cost, world: Wo
             ind_p: The parent index to which a connection with x_n is being evaluated
             tree: search tree
             cost: dictionary of costs
-            world: World through which the edge is beeing added
+            world: World through which the edge is being added
 
         Returns:
             cost of connecting to x_n from the indicated parent index
@@ -308,3 +320,71 @@ def cost_to_come(x_n: TwoDimArray, ind_p: int, tree: Tree, cost: Cost, world: Wo
     if collision_free(path=edge, world=world):
         return cost[ind_p] + edge_cost(node_1=parent_position, node_2=x_n)
     return np.inf
+
+
+############### RRT Proceedures #############
+def extend(x_rand: TwoDimArray, tree: Tree, dist: float, cost: Cost, world: World) -> tuple[TwoDimArray, int, float]:
+    """ Given a sample, x_rand, and Tree, the Extend procedure finds the closes vertex to x_rand
+        that is already in the tree and checks if a valid extension can be made from the tree
+        towards x_rand
+
+        Inputs:
+            x_rand: A random (or otherwise sampled) state to be added to the tree
+            tree: tree to be extended
+            dist: max distance from the tree for placement of the new point
+            cost: dictionary of node costs
+            world: World through which the node is being evaluated
+
+        Outputs:
+            x_new: The new node to be added to the tree
+            ind_p: The parent index for the node being added
+            cost_new: The cost to come to the new point - infinite if not valid
+    """
+    x_nearest, ind_nearest = nearest(x=x_rand, tree=tree)
+    x_new = steer(x=x_nearest, y=x_rand, dist=dist)
+    cost_new = cost_to_come(x_n=x_new, ind_p=ind_nearest, tree=tree, cost=cost, world=world)
+    return x_new, ind_nearest, cost_new
+
+def rrt(x_root: TwoDimArray,
+        X_t: StateSpace,
+        X: StateSpace,
+        dist: float,
+        bias_t: int,
+        world: World) -> tuple[list[float], list[float]]:
+    """ Performs a search from the root node to the target set using the rapidly exploring random tree algorithm
+
+        Inputs:
+            x_root: The root of the tree (i.e., the starting point of the search)
+            X_t: target set
+            X: state space
+            dist: maximum distance for extending the tree
+            bias_t: biasing of the state space
+            world: the world through which the search is being made
+
+        Returns:
+            The path through the state space from the start to the end
+                x_vec: Vector of x indices
+                y_vec: Vector of y indices
+    """
+
+    # Create the tree and cost storing structures
+    tree, cost = initialize(root=x_root)
+
+    # Loop through the space until a solution is found
+    iteration = 0 # Stores the interation count
+    while True:
+        # Extend the tree towards a biased sample
+        x_rand = biased_sample(iteration=iteration, bias_t=bias_t, X=X, X_t=X_t)
+        x_new, ind_p, cost_new = extend(x_rand=x_rand, tree=tree, dist=dist, cost=cost, world=world)
+
+        # Insert the point into the tree
+        if cost_new < np.inf:
+            node_index = insert_node(new_node=x_new, parent_ind=ind_p, tree=tree, cost=cost)
+
+            # Evaluate if the solution is complete
+            if X_t.contains(state=x_new):
+                x_vec, y_vec, _ = solution(node_index=node_index, tree=tree)
+                return (x_vec, y_vec)
+
+        # Update the interation count for the next iteration
+        iteration += 1
